d769b26789a651ef3f052f68208f96f5f104cf4e
atora761 <yuuya67200118@gmail.com>
2022-12-06 05:31:31 +0900

    単体テスト

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Diagnostics;
+using System.Linq;
 //using MISOTEN_APPLICATION.BackProcess;
 namespace MISOTEN_APPLICATION.BackProcess.Mamoru
 {
@@ -16,15 +17,13 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
         public const int point_pressure_allowerror = 50;
         
         //スレーブ始動閾値
-        public const float second_joint_threshold_pressure=400.0f;
-        public const float third_joint_threshold_pressure=400.0f;
-        public const float pressure_threshold_pressure=400.0f;
-        public const float point_pressure_threshold_pressure=400.0f;
+        public const float second_joint_threshold_pressure=0.0f;
+        public const float third_joint_threshold_pressure=0.0f;
+        public const float pressure_threshold_pressure=0.0f;
+        public const float point_pressure_threshold_pressure=0.0f;
 
         //センサの最大値
         public const int sensor_max=1024;
-
-
     }
 
     // 角度格納用構造体
@@ -45,19 +44,16 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
         public int point_pressure;
     }
     //出力値格納構造体
-    public struct GODS_SENTEMCE{
-        //指先の出力値
-        public float first_tip_pwm;
-        public float second_tip_pwm;
-        public float third_tip_pwm;
-        public float fourth_tip_pwm;
-        public float fifth_tip_pwm;
-        //手の平の出力値
-        public float first_palm_pwm;
-        public float second_palm_pwm;
-        public float third_palm_pwm;
-        public float fourth_palm_pwm;
-        public float fifth_palm_pwm;
+    public struct GODS_SENTENCE{
+        public GOD_SENTENCE first_godsentence;
+        public GOD_SENTENCE second_godsentence;
+        public GOD_SENTENCE third_godsentence;
+        public GOD_SENTENCE fourth_godsentence;
+        public GOD_SENTENCE fifth_godsentence;
+    }
+    public struct GOD_SENTENCE{
+        public int tip_pwm;
+        public int palm_pwm;
     }
     // 第一、第二、第三関節間の長さ
     public struct LENGTH{
@@ -69,9 +65,16 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
 
     public class GodHand
     {
+/*        static async Task Main(string[] args){
+            {
+                GodHand godhand=new GodHand();
+                await godhand.run();
+            }
+        }
+        */
         //動作している指
          //[0=小指][1=薬指][2=中指][3=人差し指][4=親指]
-        public Boolean[] movement_finger = new Boolean[]{false,false,false,false,false};
+        public Boolean[] movement_finger = new Boolean[]{true,true,false,false,false};
         //GodFinger[5] gotfinger=new  GodFinger;
         public GodHand(){
 
@@ -87,6 +90,7 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
                     result_master=calibration_inspection(true);
                     for(int index=0;index<5;index++){
                     //gotfinger[index].setStatingValue(result_master[index]);
+                    Console.WriteLine("task1:"+index);
                     }
                 });
                 Task task2 = Task.Run(() => {
@@ -94,6 +98,7 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
                     result_slave=calibration_inspection(false);
                     for(int index=0;index<5;index++){
                     //gotfinger[index].setStatingValue(result_slave[index]);
+                    Console.WriteLine("task2:"+index);
                     }
                 });
                 await Task.WhenAll(task1, task2);
@@ -102,6 +107,7 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
                 //スレーブ
                 result_slave=calibration_inspection(false);
                 for(int index=0;index<5;index++){
+                    Console.WriteLine("task1:"+index);
                 //gotfinger.setEndingValue(result_slave);
                 }
             }
@@ -138,29 +144,38 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
                 //Temporary_sensordate=recivedata_sensor.Thumb;
                 //小指
                 List<Task> arrayTask = new List<Task>();
-                for(int finger_count=0;finger_count<5;finger_count++){
-                    Task finger = Task.Run(() => {
+                for(int count=0;count<5;count++){
+                    int finger_count=count;
+                    Task finger = Task.Run(  () => {
                         finger_true[finger_count]=finger_inspection(Temporary_sensordate[finger_count],predate[finger_count]);
-                        receive_log[finger_count].Add(Temporary_sensordate[finger_count]);
-                        predate[finger_count]=Temporary_sensordate[finger_count];
+                        //receive_log[finger_count].Add(Temporary_sensordate[finger_count]);
+                        //predate[finger_count]=Temporary_sensordate[finger_count];
                     });
+                    arrayTask.Add(finger);
                 }
                 await Task.WhenAll(arrayTask);
+                arrayTask.Clear();
+            };
+            timer.Start();
+
+            while(true){
                 if(finger_true.All(i=>i==true)){
-                    if(truetime==0.0f){
-                        sw.Start();
-                    }else{
-                        truetime=sw.ElapsedMilliseconds;
-                        if(truetime==3000){
-                            timer.Stop();
+                        if(truetime==0.0f){
+                            sw.Start();
+                            truetime=sw.ElapsedMilliseconds;
+                        }else{
+                            truetime=sw.ElapsedMilliseconds;
+                            if(truetime>3000){
+                                timer.Stop();
+                                break;
+                            }
                         }
-                    }
                 }else{
                     sw.Restart();
                     Array.Clear(receive_log);
                 }
-            };
-            timer.Start();
+            }
+            
             for(int index=0;index<5;index++){
                 receive_log[index].ForEach(iter => {
                     avaragedate[index].second_joint+=iter.second_joint;
@@ -179,16 +194,16 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
 
         public Boolean finger_inspection(SENSOR_VALUE _sensordate,SENSOR_VALUE _predate){
             Boolean[] finger_true = new Boolean[]{false,false,false,false};  
-            if((Math.Abs(_sensordate.second_joint-_predate.second_joint))==Constants.second_joint_allowerror){
+            if((Math.Abs(_sensordate.second_joint-_predate.second_joint))<Constants.second_joint_allowerror){
                 finger_true[0]=true;
             }
-            if((Math.Abs(_sensordate.third_joint-_predate.third_joint))==Constants.third_joint_allowerror){
+            if((Math.Abs(_sensordate.third_joint-_predate.third_joint))<Constants.third_joint_allowerror){
                 finger_true[1]=true;
             }
-            if((Math.Abs(_sensordate.pressure-_predate.pressure))==Constants.pressure_allowerror){
+            if((Math.Abs(_sensordate.pressure-_predate.pressure))<Constants.pressure_allowerror){
                 finger_true[2]=true;
             }
-            if((Math.Abs(_sensordate.second_joint-_predate.point_pressure))==Constants.point_pressure_allowerror){
+            if((Math.Abs(_sensordate.second_joint-_predate.point_pressure))<Constants.point_pressure_allowerror){
                 finger_true[3]=true;
             }
             if(finger_true.All(i=>i==true)){
@@ -219,15 +234,17 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
                 //Temporary_slavedate[3]=recivedata_sensor.Index;
                 //Temporary_slavedate[4]=recivedata_sensor.Thumb;
                 List<Task> arrayTask = new List<Task>();
-                for(int finger_count=0;finger_count<5;finger_count++){
+                for(int count=0;count<5;count++){
+                    int finger_count=count;
+                    Console.WriteLine(finger_count);
                     if(movement_finger[finger_count]==false){
+                        
                         Task finger = Task.Run(() => {
-                            if(Temporary_masterdate[finger_count].second_joint>Constants.point_pressure_threshold_pressure){
+                            if(Temporary_masterdate[finger_count].second_joint==Constants.point_pressure_threshold_pressure){
                                 if(finger_count!=4){
                                     movement_finger[finger_count]=finger_starting(Temporary_slavedate[finger_count],Temporary_slavedate[finger_count+1],finger_count);
                                 }else{
                                     movement_finger[finger_count]=finger_starting(Temporary_slavedate[finger_count],Temporary_slavedate[finger_count-1],finger_count);
-
                                 }
                             }
                         });
@@ -235,19 +252,20 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
                     }
                 }
                 await Task.WhenAll(arrayTask);
+                arrayTask.Clear();
                 //全指終了
                 if(movement_finger.All(i=>i==true)){
-                    return 0;
+                    Console.WriteLine("fingerall");
+                     return 0;
                 }
             }
-            
         }
-        public Boolean  finger_starting(SENSOR_VALUE _sensordate,SENSOR_VALUE _nextsensordate ,int _finger_count){
-            float bandangle;
-            float resistangle;
-            float field_fingers;
-            float finger_height;
-            float nextfinger_height;
+        public Boolean finger_starting(SENSOR_VALUE _sensordate,SENSOR_VALUE _nextsensordate ,int _finger_count){
+            //float bandangle;
+            //float resistangle;
+            //float field_fingers;
+            //float finger_height;
+            //float nextfinger_height;
             //GodConverter godconverter=new GodConverter;
             //bandangle=godconverter.bendToAngle(_sensordate.pressure);
             //resistangle=godconverter.resistToAngle(_sensordate.point_pressure);
@@ -255,22 +273,13 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
             //nextfinger_height=forwardKinematics(Length[_finger_count],_nextsensordate.second_joint,_nextsensordate.third_joint);
             //field_fingers=godconverter.calcField(finger_height,nextfinger_height);
             //gotfinger[_finger_count].setField(field_fingers);
-
+            //gotfinger[_finger_count].setHeight(finger_height);
             return true;
         }
 
-        public int calc(){
+        public async Task<int> run(){
             SENSOR_VALUE[] Temporary_masterdate=new SENSOR_VALUE[5];
             SENSOR_VALUE[] Temporary_slavedate=new SENSOR_VALUE[5];
-            LENGTH[] Length=new LENGTH[5];
-            SENSOR_VALUE nextfinger;
-            LENGTH nextlength=new LENGTH();
-            float finger_height;
-            float finger_density;
-            float finger_field;
-            float nextfinger_height;
-            float max_power;
-            float current_power;
             //recivedata_sensor=GetMSensor();
             //Temporary_masterdate[0]=recivedata_sensor.Little;
             //Temporary_masterdate[1]=recivedata_sensor.Ring;
@@ -284,53 +293,38 @@ namespace MISOTEN_APPLICATION.BackProcess.Mamoru
             //Temporary_slavedate[2]=recivedata_sensor.Middle
             //Temporary_slavedate[3]=recivedata_sensor.Index;
             //Temporary_slavedate[4]=recivedata_sensor.Thumb;
-            for(int finger_count=0;finger_count<5;finger_count++){
-                finger_height=0.0f;
-                finger_density=0.0f;
-                finger_field=0.0f;
-                
-                if(finger_count!=4){
-                    nextfinger=Temporary_masterdate[finger_count+1];
-                    nextlength=Length[finger_count+1];
-                }else{
-                    nextfinger=Temporary_masterdate[finger_count-1];
-                    nextlength=Length[finger_count-1];
-                }
+            GOD_SENTENCE[] finger_power =new GOD_SENTENCE[5];
+            GODS_SENTENCE gods_senten=new GODS_SENTENCE();
+            List<Task> arrayTask = new List<Task>();
+            for(int count=0;count<5;count++){
+                int finger_count =count;
                 if(movement_finger[finger_count]==true){
-                    //ゴッドコンバータインスタンス化
-                    //GodConverter godconverter=new GodConverter;
-                    //マスター曲げセンサの値取得
-                    //Temporary_masterdate[finger_count].second_joint=godconverter.bendToAngle(Temporary_masterdate[finger_count].second_joint);
-                    //マスター可変抵抗の値取得
-                    //Temporary_masterdate[finger_count].third_joint=godconverter.resistToAngle(Temporary_masterdate[finger_count].third_joint);
-                    //ゴッドフィンガーにセンサーデータを格納
-                    //gotfinger[finger_count].setSensorValue(Temporary_masterdate[finger_count]);
-                    //関節間の指の長さ取得
-                    //Length[finger_count]=getLength(finger_count);
-                    
-                    //比率計算用最大値計算
-                    //指の高さを順運動学で取得
-                    //finger_height=godconverter.forwardKinematics(Length[finger_count],Temporary_masterdate[finger_count].second_joint,Temporary_masterdate[finger_count].third_joint);
-                    //nextfinger_height=godconverter.forwardKinematics(nextlength[finger_count],nextfinger.second_joint,nextfinger.third_joint);                
-                    //モノの密度計算
-                    //finger_density=godconverter.calcDensity(finger_height,sensor_max,sensor_max);
-                    //マスター現在の面積計算
-                    //finger_field=godconverter.calcField(finger_height,nextfinger_height);
-                    //max_power=finger_density*ratiio(gotfinger[finger_count],finger_field); 
-
-                    //比率計算用現在値計算
-                    //指の高さを順運動学で取得
-                    //finger_height=godconverter.forwardKinematics(Length[finger_count],Temporary_masterdate[finger_count].second_joint,Temporary_masterdate[finger_count].third_joint);
-                    //nextfinger_height=godconverter.forwardKinematics(nextlength[finger_count],nextfinger.second_joint,nextfinger.third_joint);                
-                    //モノの密度計算
-                    //finger_density=godconverter.calcDensity(finger_height,Temporary_masterdate[finger_count].pressure,Temporary_masterdate[finger_count].point_pressure);
-                    //マスター現在の面積計算
-                    //finger_field=godconverter.calcField(finger_height,nextfinger_height);
-                    //current_power=finger_density*ratiio(gotfinger[finger_count],finger_field);         
-
-                    //pwmの送信           
+                    Console.WriteLine(finger_count);
+                    Task finger = Task.Run(() => {
+                        if(finger_count!=4){
+                            //gotfinger[finger_count].master_data=setSensorValue(Temporary_masterdate[finger_count]);
+                            //gotfinger[finger_count].slave_data=setSensorValue(Temporary_masterdate[finger_count]);
+                            //gotfinger[finger_count+1].slave_data=setSensorValue(Temporary_masterdate[finger_count]);
+                            //finger_power[finger_count]=calc(gotfinger[finger_count],gotfinger[finger_count+1])
+                        }else{
+                            //gotfinger[finger_count].master_data=setSensorValue(Temporary_masterdate[finger_count]);
+                            //gotfinger[finger_count].slave_data=setSensorValue(Temporary_masterdate[finger_count]);
+                            //gotfinger[finger_count-1].slave_data=setSensorValue(Temporary_masterdate[finger_count]);
+                            //finger_power[finger_count]=calc(gotfinger[finger_count],gotfinger[finger_count-1])
+                        }
+                    });
+                    arrayTask.Add(finger);
                 }
             }
+            await Task.WhenAll(arrayTask);
+            arrayTask.Clear();
+            gods_senten.first_godsentence=finger_power[0];
+            gods_senten.second_godsentence=finger_power[1];
+            gods_senten.third_godsentence=finger_power[2];
+            gods_senten.fourth_godsentence=finger_power[3];
+            gods_senten.fifth_godsentence=finger_power[4];
+
+            //スレーブに出力値を送信
             return 0;
         }
     }
